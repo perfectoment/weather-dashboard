170b84f91e1b7ba405c43d1c332a60d267272a72
Lee Marvin <ryenem8@gmail.com>
2020-09-28 18:53:22 -0700

    got intial dashboard to read

--- script.js
+++ script.js
@@ -2,48 +2,41 @@ $(document).ready(function() {
   $("#search-button").on("click", function() {
     var formSearch = $("#search-value").val();
     $("#search-value").val("");
-
-    searchWeather(formSearch);
+    weatherMap(formSearch);
   });
-
-  $(".history").on("click", "li", function() {
-    searchWeather($(this).text());
- 
-
-
+  $(".city-list").on("click", "li", function() {
+    weatherMap($(this).text());
   });
   function addCity(text) {
     var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
-    $(".history").append(li);
+    $(".city-list").append(li);
   }
-
-  function searchWeather(s) {
+  function weatherMap(formSearch) {
     $.ajax({
       type: "GET",
       url: "http://api.openweathermap.org/data/2.5/weather?q=" + formSearch + "&appid=25e06f9ff8307c93ac15e24fa0a3c459&units=imperial",
       dataType: "json",
-      success: function(data) {
-        // create history link for this search
-        if (history.indexOf(formSearch) === -1) {
-          history.push(formSearch);
-          window.localStorage.setItem("history", JSON.stringify(history));
+      success: function(response) {
+       
     
-          addCity(formSearch);
-        }
-        $("#today").empty(); 
-
-
-
-
-
-
-
-
-
-
+        addCity(formSearch);
         
+        
+ $("#card-title").empty();
+
+ $(".card-title").text(response.name + " (" + new Date().toLocaleDateString() + ")");
+ $("#wind").text("Wind " + response.wind.speed + "MPH");
+ $("#temperature").text("Temperature " + response.main.temp + "F");
+ $("#humidity").text("Humidity " + response.main.humidity + "%")
+
       }
-})}
+      
+
+})
+
+var formStorage =JSON
+
+}
 
 
 
