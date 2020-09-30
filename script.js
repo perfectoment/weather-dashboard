f4e019f8d78df7f10b95ae2e7fde62278714f949
Lee Marvin <ryenem8@gmail.com>
2020-09-29 20:32:27 -0700

    finally added 5 day forecast

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
+
 function forecast5day(formSearch) {
   $.ajax({
     type: "GET",
     url: "http://api.openweathermap.org/data/2.5/forecast?q=" + formSearch + "&appid=25e06f9ff8307c93ac15e24fa0a3c459&units=imperial",
     dataType: "json",
     success: function(response) {
-      for(var i = 0; i < response.list.length; i++) {
-        if (response.list[i].dt_txt.indexOf("15:00:00") !== -1) {
-          // console.log(new Date(response.list[i].dt_txt).toLocaleDateString())
-           var title = (response.list[i].dt_txt.indexOf("15:00:00"))
+      for(var i = 0; i < 40; i=i+8) {
+        var cardBody = $("<div>");
+        var data = response.list[i];
+        var currentDate = $("<p>").text(data.dt_txt.substring(0, 10))
+        var temperature5day = $("<p>").text("Temperature: " + data.main.temp);
+        var humidity5day = $("<p>").text("Humidity: " + data.main.humidity);
+        var iconcode = data.weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png"
+        var imageTag = $("<img>");
+        imageTag.attr("src", iconurl);
+        var day5 = $("#5day");
+        cardBody.attr("card", "card card-body border border-secondary")
+        cardBody.append(imageTag, currentDate, temperature5day, humidity5day);
+        day5.append(cardBody);
+
+      
+
           
-         $(".dayHeadline").text(title[0]);
-        
-        // $(".5dayheadline").text(new Date(response.list[i].dt_txt).toLocaleDateString())
-
-            // console.log(new Date(response.list[i].dt_txt).toLocaleDateString())
-            
-
-        
+    
       
-
-  }
-      
-}   
+}   }
   
-  }}
+  }
   )}
 $(document).ready(function() {
   
@@ -45,7 +49,8 @@ $(document).ready(function() {
       url: "http://api.openweathermap.org/data/2.5/weather?q=" + formSearch + "&appid=25e06f9ff8307c93ac15e24fa0a3c459&units=imperial",
       dataType: "json",
       success: function(response) {
-     forecast5day(formSearch);    
+     forecast5day(formSearch);  
+     console.log(response);  
  addCity(formSearch);
  $(".card-title").text(response.name + " (" + new Date().toLocaleDateString() + ")");
  $("#wind").text("Wind " + response.wind.speed + "MPH");
@@ -54,8 +59,9 @@ $(document).ready(function() {
  var iconcode = response.weather[0].icon;
  var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png"
  $('#img').attr('src', iconurl);
-      }
       
+      
+      } 
 
 });
 
