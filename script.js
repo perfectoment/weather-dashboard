b70e302d412a6209a54c539003591d656bedc553
Lee Marvin <ryenem8@gmail.com>
2020-09-29 17:54:59 -0700

    trying to get 5 day forcast going

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+function forecast5day(formSearch) {
+  $.ajax({
+    type: "GET",
+    url: "http://api.openweathermap.org/data/2.5/forecast?q=" + formSearch + "&appid=25e06f9ff8307c93ac15e24fa0a3c459&units=imperial",
+    dataType: "json",
+    success: function(response) {
+      for(var i = 0; i < response.list.length; i++) {
+        if (response.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+          // console.log(new Date(response.list[i].dt_txt).toLocaleDateString())
+           var title = (response.list[i].dt_txt.indexOf("15:00:00"))
+          
+         $(".dayHeadline").text(title[0]);
+        
+        // $(".5dayheadline").text(new Date(response.list[i].dt_txt).toLocaleDateString())
+
+            // console.log(new Date(response.list[i].dt_txt).toLocaleDateString())
+            
+
+        
+      
+
+  }
+      
+}   
+  
+  }}
+  )}
 $(document).ready(function() {
+  
   $("#searchBtn").on("click", function() {
     var formSearch = $("#searchForm").val();
     $("#searchForm").val("");
@@ -17,29 +45,20 @@ $(document).ready(function() {
       url: "http://api.openweathermap.org/data/2.5/weather?q=" + formSearch + "&appid=25e06f9ff8307c93ac15e24fa0a3c459&units=imperial",
       dataType: "json",
       success: function(response) {
-       
+     forecast5day(formSearch);    
  addCity(formSearch);
  $(".card-title").text(response.name + " (" + new Date().toLocaleDateString() + ")");
  $("#wind").text("Wind " + response.wind.speed + "MPH");
  $("#temperature").text("Temperature " + response.main.temp + "F");
  $("#humidity").text("Humidity " + response.main.humidity + "%")
-
+ var iconcode = response.weather[0].icon;
+ var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png"
+ $('#img').attr('src', iconurl);
       }
       
 
 });
-function forecast5day(formSearch) {
-  $.ajax({
-    type: "GET",
-    url: "http://api.openweathermap.org/data/2.5/forecast?q=" + formSearch + "&appid=25e06f9ff8307c93ac15e24fa0a3c459&units=imperial",
-    dataType: "json",
-    success: function(response) {
-      console.log(response);
 
-  } 
-  
-  }
-)}
 
 
 
